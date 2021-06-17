22b7da5bffff071e4d0c064e41ccb5e686046ff7
tttkouyou <bjkt1290248@gn.iwasaki.ac.jp>
2021-06-17 14:36:48 +0900

    [CREATE] StatePattern を作成しました

---
+++ src/NeteruState.java
@@ -0,0 +1,19 @@
+public class NeteruState extends DogState {
+	private static NeteruState s = new NeteruState();
+	private NeteruState() {}
+
+	public static DogState getInstance() {
+		return s;
+	}
+
+	public void tukareta(Dog moto) {
+		moto.changeState(IrairaState.getInstance());
+	}
+
+	public void tabeta(Dog moto) {
+	}
+
+	public String toString() {
+		return "寝てる状態";
+	}
+}
